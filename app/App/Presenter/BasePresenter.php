fd29e545d09bef74bc5d1760364ad10e49d5b209
Martin Endler <pokusew@seznam.cz>
2021-12-12 02:51:35 +0100

    Add declare(strict_types=1) to every PHP file

--- app/App/Presenter/BasePresenter.php
+++ app/App/Presenter/BasePresenter.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Presenter;
 
 class BasePresenter extends \Core\UI\Presenter
--- app/App/Presenter/SignUpPresenter.php
+++ app/App/Presenter/SignUpPresenter.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Presenter;
 
 class SignUpPresenter extends BasePresenter

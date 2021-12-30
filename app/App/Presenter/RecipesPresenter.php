602d8efae89a6c1fdf832484af76a5728397e250
Martin Endler <pokusew@seznam.cz>
2021-12-30 16:25:06 +0100

    Add PDO demo

--- app/App/Presenter/RecipesPresenter.php
+++ app/App/Presenter/RecipesPresenter.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Presenter;
 
+use PDO;
+use PDOException;
+
 class RecipesPresenter extends BasePresenter
 {
 
@@ -14,7 +17,20 @@ class RecipesPresenter extends BasePresenter
 
 	public function action(): void
 	{
-
+		// just a database demo
+		try {
+			$dbh = new PDO($this->config->databaseDsn);
+			if (!$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)) {
+				throw new \RuntimeException('Could not set PDO::ATTR_ERRMODE to PDO::ERRMODE_EXCEPTION.');
+			}
+			foreach ($dbh->query('SELECT * from FOO') as $row) {
+				dump($row);
+			}
+			$dbh = null;
+		} catch (PDOException $e) {
+			// let the core app handle the exception
+			throw $e;
+		}
 	}
 
 }

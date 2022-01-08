a26c2612694c3d956217d5f3292aa86db32c2e09
Martin Endler <pokusew@seznam.cz>
2022-01-08 05:01:36 +0100

    Improve PHPDoc in Core\App

--- app/Core/App.php
+++ app/Core/App.php
@@ -13,6 +13,9 @@ use Core\UI\Presenter;
 use Tracy\Debugger;
 use Tracy\ILogger;
 
+/**
+ * Front controller.
+ */
 class App
 {
 
@@ -37,6 +40,16 @@ class App
 		$this->config = $config;
 	}
 
+	/**
+	 * Tries to create a new instance of the presenter by its short name.
+	 *
+	 * The FQN of the presenter is constructed as follows:
+	 * `{\Core\Config::$presenterNamespace}\{$presenterName}Presenter`.
+	 *
+	 * @param string $presenterName the presenter class short name (without namespace and without `Presenter` suffix)
+	 * @return Presenter a new instance of the presenter
+	 * @see Config::$presenterNamespace
+	 */
 	protected function createPresenter(string $presenterName): Presenter
 	{
 		$presenterClassName = $this->config->presenterNamespace . '\\' . $presenterName . 'Presenter';
@@ -54,7 +67,16 @@ class App
 	}
 
 	/**
-	 * @throws BadRequestException
+	 * Tries to processes the HTTP request and send an HTTP response.
+	 *
+	 * First, it tries to match the HTTP request to a route using the router's {@see Router::match()}.
+	 * Then, on successful match, it instantiate the corresponding presenter
+	 * and calls its {@see Presenter::run()} method.
+	 * If the presenter generated a non-null response {@see \Core\Response\Response}, it attempts to send it.
+	 *
+	 * It does not do any error handling (it is done by {@see App::run()}.
+	 *
+	 * @throws BadRequestException when there is no route for HTTP request (i.e. the router returns no match)
 	 */
 	protected function processRequest(): void
 	{
@@ -73,11 +95,29 @@ class App
 		}
 	}
 
+	/**
+	 * Runs the app.
+	 *
+	 * It calls {@see processRequest()} which does the actual work.
+	 *
+	 * When an exception occurs during {@see processRequest()},
+	 * it is automatically caught, and the ErrorPresenter
+	 * is instantiated and the exception is handed to it.
+	 *
+	 * If another exception occurs during the exception processing in the error presenter,
+	 * it is also automatically caught, it is logged using {@see Debugger::log()}
+	 * with the {@see ILogger::CRITICAL} level, and then a generic HTTP 500 Server error response is sent.
+	 *
+	 * **NOTE:** In the development mode (i.e. {@see Config::isModeDevelopment()}),
+	 * all the exceptions handling is disabled, so that we can leverage the Tracy debugger
+	 * (and its built-in blue screen).
+	 *
+	 * @return void
+	 */
 	public function run(): void
 	{
 		// during the development, let the Tracy handle all exceptions (i.e. show the blue screen)
 		if ($this->config->isModeDevelopment()) {
-			/** @noinspection PhpUnhandledExceptionInspection */
 			$this->processRequest();
 			return;
 		}
